25d6acd493f9dc2d673195b322a5e9e77844c909
Kunal1762 <kunalbari098@gmail.com>
2026-04-13 15:06:14 +0530

    added update and delete

--- index.js
+++ index.js
@@ -26,6 +26,31 @@ app.get("/list", (req,res)=>{
     res.json(todolist);
 })
 
+app.put("/update/:id",(req,res)=>{
+    const id=parseInt(req.params.id);
+    const todo=todolist.find(t=>t.id===id);
+
+    if(!todo){
+        return res.status(404).json({error:"Not Found"});
+    }
+    if(req.body.text!==undefined){
+        todo.text=req.body.text;
+    }
+    if(req.body.completed!==undefined){
+        todo.completed=req.body.completed;
+    }
+    res.json(todo);
+})
+app.delete("/remove/:id",(req,res)=>{
+    const id=parseInt(req.params.id);
+    const index=todolist.findIndex(t=>t.id===id);
+    let deleted;
+    if(index!=-1){
+        deleted=todolist.splice(index,1);
+    }
+    res.json(deleted);
+})
+
 app.listen(PORT, ()=>{
     console.log(`server is listening on port ${PORT}.`);
 });
